caeb192b1d0794d3afa4ff3c5d2b5795e2b14d49
Sven Heising <sven.heising@gmail.com>
2026-03-22 08:40:11 +0100

    refactor(4.1): Repositories mit domänenrelevanten Queries befüllt
    Alle drei leeren Repositories erhalten sinnvolle Methoden:
    
    TeamRepository:
    - findByStatus(StatusEnum): Ampel-Filter
    - findAllOrderedByName(): alphabetische Sortierung
    
    InitiativeRepository:
    - findByTeam(int): Initiatives eines Teams
    - findByStatus(StatusEnum): Ampel-Filter
    - findCritical(): projektstatus=kritisch, nach frist sortiert
    - findWithoutTeam(): teamlose Initiatives
    
    NichtVergessenRepository:
    - findAllOrderedByTitle(): alphabetische Sortierung
    
    Alle 25 Tests grün.

--- src/Repository/TeamRepository.php
+++ src/Repository/TeamRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Team;
+use App\Enum\StatusEnum;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,5 +15,20 @@ class TeamRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Team::class);
+    }
+
+    /** Alle Teams mit einem bestimmten Ampel-Status. */
+    public function findByStatus(StatusEnum $status): array
+    {
+        return $this->findBy(['status' => $status]);
+    }
+
+    /** Teams alphabetisch nach Name sortiert. */
+    public function findAllOrderedByName(): array
+    {
+        return $this->createQueryBuilder('t')
+            ->orderBy('t.name', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 }
